728f04b57b2b244d46628d4718edaace8f05383a
HomeBjo <lala2-wuppertal@hotmail.com>
2025-03-13 11:44:01 +0100

    Added project setup

---
+++ src/main/java/com/join/backend/JoinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.join.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/join/backend/JoinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.join.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
