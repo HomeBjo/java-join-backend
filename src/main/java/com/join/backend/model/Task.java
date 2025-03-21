1e3d8a7ac4046a986411185c17ecc68cb1d37efd
HomeBjo <lala2-wuppertal@hotmail.com>
2025-03-21 10:29:44 +0100

    add model and repository

---
+++ src/main/java/com/join/backend/model/Task.java
@@ -0,0 +1,42 @@
+package com.join.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String status;
+
+    public Task() {}
+
+    public Task(String title, String status) {
+        this.title = title;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
